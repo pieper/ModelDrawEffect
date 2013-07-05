8cddb6d8e3da10504b5c2a687c3550f269db9a35
Steve Pieper <pieper@isomics.com>
2013-07-05 19:10:23 -0400

    ENH: set up basic framework for reloadable editor effect
    Now when you reload (or reload and test) the new implementation
    of the effect is registered with the editor so that the tool
    button will create a new instance of the new version of the code.

--- ModelDrawEffect.py
+++ ModelDrawEffect.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 from __main__ import vtk, qt, ctk, slicer
 import EditorLib
 from EditorLib.EditOptions import HelpButton
@@ -119,7 +120,7 @@ class ModelDrawEffectTool(LabelEffect.LabelEffectTool):
       sliceLogic = self.sliceWidget.sliceLogic()
       logic = ModelDrawEffectLogic(sliceLogic)
       logic.apply(xy)
-      print("Got a %s at %s in %s", (event,str(xy),self.sliceWidget.sliceLogic().GetSliceNode().GetName()))
+      print("NEW!!!! Got a %s at %s in %s" % (event,str(xy),self.sliceWidget.sliceLogic().GetSliceNode().GetName()))
       self.abortEvent(event)
     else:
       pass
@@ -172,13 +173,6 @@ class ModelDrawEffectExtension(LabelEffect.LabelEffect):
     self.tool = ModelDrawEffectTool
     self.logic = ModelDrawEffectLogic
 
-""" Test:
-
-sw = slicer.app.layoutManager().sliceWidget('Red')
-import EditorLib
-pet = EditorLib.ModelDrawEffectTool(sw)
-
-"""
 
 #
 # ModelDrawEffect
@@ -190,7 +184,7 @@ class ModelDrawEffect:
   as a loadable scripted module
   """
   def __init__(self, parent):
-    parent.title = "Editor ModelDrawEffect Effect"
+    parent.title = "Editor ModelDraw Effect"
     parent.categories = ["Developer Tools.Editor Extensions"]
     parent.contributors = ["Steve Pieper (Isomics)"] # insert your name in the list
     parent.helpText = """
@@ -198,17 +192,12 @@ class ModelDrawEffect:
     """
     parent.acknowledgementText = """
     This editor extension was developed by
-    <Author>, <Institution>
-    based on work by:
     Steve Pieper, Isomics, Inc.
-    based on work by:
-    Jean-Christophe Fillion-Robin, Kitware Inc.
-    and was partially funded by NIH grant 3P41RR013218.
+    and was partially funded by NIH grant 3P41RR013218 and by Novartis.
     """
 
-    # TODO:
-    # don't show this module - it only appears in the Editor module
-    #parent.hidden = True
+    parent.hidden = False
+
 
     # Add this extension to the editor's list for discovery when the module
     # is created.  Since this module may be discovered before the Editor itself,
@@ -227,9 +216,47 @@ class ModelDrawEffectWidget:
   def __init__(self, parent = None):
     self.parent = parent
 
-  def setup(self):
-    # don't display anything for this widget - it will be hidden anyway
+    if not parent:
+      self.parent = slicer.qMRMLWidget()
+      self.parent.setLayout(qt.QVBoxLayout())
+      self.parent.setMRMLScene(slicer.mrmlScene)
+    else:
+      self.parent = parent
+    self.layout = self.parent.layout()
+    if not parent:
+      self.setup()
+      self.parent.show()
+
+  def cleanup(self):
     pass
+
+  def setup(self):
+    """For development, add a reloadable section"""
+
+    #
+    # Reload and Test area
+    #
+    reloadCollapsibleButton = ctk.ctkCollapsibleButton()
+    reloadCollapsibleButton.text = "Reload && Test"
+    self.layout.addWidget(reloadCollapsibleButton)
+    reloadFormLayout = qt.QFormLayout(reloadCollapsibleButton)
+
+    # reload button
+    # (use this during development, but remove it when delivering
+    #  your module to users)
+    self.reloadButton = qt.QPushButton("Reload")
+    self.reloadButton.toolTip = "Reload this module."
+    self.reloadButton.name = "ModelDrawEffect Reload"
+    reloadFormLayout.addWidget(self.reloadButton)
+    self.reloadButton.connect('clicked()', self.onReload)
+
+    # reload and run specific tests
+    scenarios = ("Basic",)
+    for scenario in scenarios:
+      button = qt.QPushButton("Reload and Test %s" % scenario)
+      button.toolTip = "Reload this module and then run the %s self test." % scenario
+      reloadFormLayout.addWidget(button)
+      button.connect('clicked()', lambda s=scenario: self.onReloadAndTest(scenario=s))
 
   def enter(self):
     pass
@@ -237,4 +264,124 @@ class ModelDrawEffectWidget:
   def exit(self):
     pass
 
+  def onReload(self,moduleName="ModelDrawEffect"):
+    """Generic reload method for any scripted module.
+    ModuleWizard will subsitute correct default moduleName.
+    """
+    import imp, sys, os, slicer
+
+    widgetName = moduleName + "Widget"
+
+    # reload the source code
+    # - set source file path
+    # - load the module to the global space
+    filePath = eval('slicer.modules.%s.path' % moduleName.lower())
+    p = os.path.dirname(filePath)
+    if not sys.path.__contains__(p):
+      sys.path.insert(0,p)
+    fp = open(filePath, "r")
+    globals()[moduleName] = imp.load_module(
+        moduleName, fp, filePath, ('.py', 'r', imp.PY_SOURCE))
+    fp.close()
+
+    # rebuild the widget
+    # - find and hide the existing widget
+    # - create a new widget in the existing parent
+    parent = slicer.util.findChildren(name='%s Reload' % moduleName)[0].parent().parent()
+    for child in parent.children():
+      try:
+        child.hide()
+      except AttributeError:
+        pass
+    # Remove spacer items
+    item = parent.layout().itemAt(0)
+    while item:
+      parent.layout().removeItem(item)
+      item = parent.layout().itemAt(0)
+
+    # delete the old widget instance
+    if hasattr(globals()['slicer'].modules, widgetName):
+      getattr(globals()['slicer'].modules, widgetName).cleanup()
+
+    # create new widget inside existing parent
+    globals()[widgetName.lower()] = eval(
+        'globals()["%s"].%s(parent)' % (moduleName, widgetName))
+    globals()[widgetName.lower()].setup()
+    setattr(globals()['slicer'].modules, widgetName, globals()[widgetName.lower()])
+
+    # special for Editor Effects - register as the new implementation of effect
+    slicer.modules.editorExtensions['ModelDrawEffect'] = ModelDrawEffectExtension
+
+  def onReloadAndTest(self,moduleName="ModelDrawEffect",scenario=None):
+    try:
+      self.onReload()
+      evalString = 'globals()["%s"].%sTest()' % (moduleName, moduleName)
+      tester = eval(evalString)
+      tester.runTest(scenario=scenario)
+    except Exception, e:
+      import traceback
+      traceback.print_exc()
+      qt.QMessageBox.warning(slicer.util.mainWindow(),
+          "Reload and Test", 'Exception!\n\n' + str(e) + "\n\nSee Python Console for Stack Trace")
+
+class ModelDrawEffectTest(unittest.TestCase):
+  """
+  This is the test case for your scripted module.
+  """
+
+  def delayDisplay(self,message,msec=1000):
+    """This utility method displays a small dialog and waits.
+    This does two things: 1) it lets the event loop catch up
+    to the state of the test so that rendering and widget updates
+    have all taken place before the test continues and 2) it
+    shows the user/developer/tester the state of the test
+    so that we'll know when it breaks.
+    """
+    print(message)
+    self.info = qt.QDialog()
+    self.infoLayout = qt.QVBoxLayout()
+    self.info.setLayout(self.infoLayout)
+    self.label = qt.QLabel(message,self.info)
+    self.infoLayout.addWidget(self.label)
+    qt.QTimer.singleShot(msec, self.info.close)
+    self.info.exec_()
+
+  def setUp(self):
+    """ Do whatever is needed to reset the state - typically a scene clear will be enough.
+    """
+    slicer.mrmlScene.Clear(0)
+
+  def runTest(self,scenario=None):
+    """Run as few or as many tests as needed here.
+    """
+    self.setUp()
+    if scenario == "Basic":
+      self.test_ModelDrawEffect1()
+    elif scenario == "Thin Plate":
+      self.test_ModelDrawEffect3()
+    else:
+      self.test_ModelDrawEffect1()
+      #self.test_ModelDrawEffect2()
+      #self.test_ModelDrawEffect3()
+
+  def test_ModelDrawEffect1(self):
+    """
+    This tests basic landmarking with two volumes
+    """
+
+    self.delayDisplay("Starting test_ModelDrawEffect1")
+    #
+    # first, get some data
+    #
+    import SampleData
+    sampleDataLogic = SampleData.SampleDataLogic()
+    mrHead = sampleDataLogic.downloadMRHead()
+    self.delayDisplay('Two data sets loaded')
+
+    w = slicer.modules.ModelDrawEffectWidget
+
+    # TODO: write a test
+    #logic = ModelDrawEffectLogic()
+
+    self.delayDisplay('test_ModelDrawEffect1 passed!')
 
